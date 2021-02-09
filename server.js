5abd8d3b06a3196447e9fcbfe3939dcb3b08ee15
Agustin <agu@cursosit.com>
2021-02-09 12:37:40 -0300

    incorrectly named file was fixed

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
-});
-
-
-
-
 const db = require("./app/models");
 db.sequelize.sync();
 
@@ -31,13 +23,18 @@ db.sequelize.sync();
 
 // db.sequelize.sync({ force: true }).then(() => {
 //     console.log("Drop and re-sync db.");
-//   });
+// });
 
 
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+require("./app/routes/tutorial.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8087;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
